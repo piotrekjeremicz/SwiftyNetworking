49d84d30afc3db86ac0fc5a2a8a31f06f38b8764
Piotrek Jeremicz <piotrek@jeremicz.com>
2023-04-02 14:01:36 +0200

    Create `Configuration` to replace `Content`

---
+++ Sources/Networking/Configuration.swift
@@ -0,0 +1,38 @@
+//
+//  Configuration.swift
+//  
+//
+//  Created by Piotrek Jeremicz on 02/04/2023.
+//
+
+import Foundation
+
+public struct Configuration {
+    public var path: String
+    public var service: Service
+    public var method: Method
+    
+    public var body: (any Codable)?
+    public var headers: [any KeyValueProvider]?
+    public var queryItems: [any KeyValueProvider]?
+    
+    public var bodyEncoder: any DataEncoder
+    
+    public init(
+        path: [String],
+        service: Service,
+        method: Method,
+        body: (Codable)? = nil,
+        headers: [any KeyValueProvider]? = nil,
+        queryItems: [any KeyValueProvider]? = nil,
+        bodyEncoder: DataEncoder
+    ) {
+        self.path = path.joined(separator: "/")
+        self.service = service
+        self.method = method
+        self.body = body
+        self.headers = headers
+        self.queryItems = queryItems
+        self.bodyEncoder = bodyEncoder
+    }
+}

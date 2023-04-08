718c0b74e777ad6a52737d33a981caa3d76296f7
Piotrek Jeremicz <piotrek@jeremicz.com>
2023-04-08 15:21:18 +0200

    Implement changes in `Example` project

---
+++ Examples/swifty-networking-sample-app/Models.swift
@@ -0,0 +1,20 @@
+//
+//  Models.swift
+//  swifty-networking-sample-app
+//
+//  Created by Piotrek Jeremicz on 08/04/2023.
+//
+
+import Foundation
+
+struct ExampleResponseModel: Codable {
+    let id: UUID
+    let title: String
+    let description: String
+}
+
+struct ExampleErrorModel: Codable {
+    let id: UUID
+    let statusCode: Int
+    let message: String
+}
--- Examples/swifty-networking-sample-app/main.swift
+++ Examples/swifty-networking-sample-app/main.swift
@@ -14,5 +14,13 @@ let service = BackendService()
 struct GetExampleRequest: Request {
     var body: some Request {
         Get("foo", "bar", from: service)
+            .response(ExampleResponseModel.self)
+            .error(ExampleErrorModel.self)
+            .body("This is some text")
+            .body {
+                Key("foo", value: "bar")
+            }
     }
 }
+
+let getRequest = GetExampleRequest()

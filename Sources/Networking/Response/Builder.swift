e399c7d2f6f7127018205abf425464ca8b880d30
Piotr Jeremicz <piotrek@jeremicz.com>
2023-05-12 10:40:12 +0200

    Create `RequestBuilder` to store all variables that should be called at the end

---
+++ Sources/Networking/Response/Builder.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Piotrek Jeremicz on 12/05/2023.
+//
+
+import Foundation
+
+public struct ResponseBuilder<Body: Codable> {
+    typealias Body = Body
+    
+    let afterAuthorization: ((_ response: Body, _ store: AuthorizationStore) -> Void)?
+    
+    public init(afterAuthorization: ((_ response: Body, _ store: AuthorizationStore) -> Void)? = nil) {
+        self.afterAuthorization = afterAuthorization
+    }
+    
+    func resolve<R: Request>(result: (data: Data, response: URLResponse), request: R) throws -> Response<Body> {
+        let response = try Response<Body>(result, from: request)
+        
+        if let store = request.configuration?.service.authorizationProvider?.store {
+            request.builder.afterAuthorization?(response.body as! R.ResponseBody, AuthorizationStore())
+        }
+        
+        return response
+    }
+}

4b59ad4299a724ec4b73400bea6b80206582dc9f
Piotrek Jeremicz <piotrek@jeremicz.com>
2023-05-17 10:21:27 +0200

    Improve `JsonBuilder`

--- Sources/Networking/Types/Json.swift
+++ Sources/Networking/Types/Json.swift
@@ -28,7 +28,23 @@ public struct Json {
 
 @resultBuilder
 public struct JsonBuilder {
-	public static func buildBlock(_ components: any JsonKey...) -> [any JsonKey] {
-		components
-	}
+    public static func buildBlock(_ components: any JsonKey...) -> [any JsonKey] {
+        components
+    }
+
+    public static func buildEither(first component: [any JsonKey]) -> [any JsonKey] {
+        component
+    }
+
+    public static func buildEither(second component: [any JsonKey]) -> [any JsonKey] {
+        component
+    }
+
+    public static func buildArray(_ components: [[any JsonKey]]) -> [any JsonKey] {
+        components.flatMap { $0 }
+    }
+
+    public static func buildOptional(_ component: [any JsonKey]?) -> [any JsonKey] {
+        component ?? []
+    }
 }

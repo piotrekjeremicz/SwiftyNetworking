83a7d182a10184b8d3aaa2111583a6711fb90bfa
Piotr Jeremicz <piotrek@jeremicz.com>
2023-09-22 09:27:41 +0200

    Added macro as target
    The second step was to copy the `Package.swift` configuration to the current Swifty Networking project.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
-// swift-tools-version: 5.7.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "SwiftyNetworking",
@@ -13,15 +14,48 @@ let package = Package(
         .library(
             name: "Networking",
             targets: ["Networking"]),
+        .library(
+            name: "MyMacro",
+            targets: ["MyMacro"]
+        ),
+        .executable(
+            name: "MyMacroClient",
+            targets: ["MyMacroClient"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", from: "5.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "10.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
     ],
     targets: [
-        .target(
-            name: "Networking",
-            dependencies: []),
+        .target(name: "Networking", dependencies: []),
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        // Macro implementation that performs the source transformation of a macro.
+        .macro(
+            name: "MyMacroMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+
+        // Library that exposes a macro as part of its API, which is used in client programs.
+        .target(name: "MyMacro", dependencies: ["MyMacroMacros"]),
+
+        // A client of the library, which is able to use the macro in its own code.
+        .executableTarget(name: "MyMacroClient", dependencies: ["MyMacro"]),
+        
+        // A test target used to develop the macro implementation.
+        .testTarget(
+            name: "MyMacroTests",
+            dependencies: [
+                "MyMacroMacros",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ]
+        ),
+        
         //TODO: Temporary target removal
 //        .testTarget(
 //            name: "NetworkingTests",

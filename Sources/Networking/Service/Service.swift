c323eff0a67be4900d884b39488cdd3c740f2b7c
Piotr Jeremicz <piotrek@jeremicz.com>
2025-10-10 22:34:02 +0200

    Improved Request and Response logging

---
+++ Sources/Networking/Service/Service.swift
@@ -0,0 +1,23 @@
+//
+//  Service.swift
+//  SwiftyNetworking
+//
+//  Created by Piotrek Jeremicz on 28.09.2025.
+//
+
+import Foundation
+import OSLog
+
+// TODO: BeforeEach, AfterEach, AutorizationProvider
+public protocol Service: Sendable {
+    var baseURL: String { get }
+    var logger: Logger? { get }
+    
+    var requestBodyEncoder: any TopLevelEncoder { get }
+    var responseBodyDecoder: any TopLevelDecoder { get }
+}
+
+public extension Service {
+    var logger: Logger? { nil }
+}
+

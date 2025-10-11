86232240bc328b346c1dfe18267c19f8c87a3862
Piotr Jeremicz <piotrek@jeremicz.com>
2025-10-11 09:23:29 +0200

    Added template to the project

---
+++ Templates/Request.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import Networking
+
+@Request
+struct ___FILEBASENAMEASIDENTIFIER___ {
+    let service: Service
+    
+    var body: some Request {
+        /*@START_MENU_TOKEN@*/Get/*[["Delete","Path","Put","Post","Get"],[[[-1,4],[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/(<#T##path: String...##String#>, from: service)
+    }
+}
